ad851060ffb23d2b843ea227e4ab7b3ec6e1bfac
Vinicius Tardelli Andreoni <vinicius.andreoni@f1rst.com.br>
2024-05-14 14:26:31 -0300

    Criado Handlers personalizados para exceções

--- src/main/java/br/com/patitas/app/controller/handler/ControllerExceptionHandler.java
+++ src/main/java/br/com/patitas/app/controller/handler/ControllerExceptionHandler.java
@@ -1,6 +1,9 @@
 package br.com.patitas.app.controller.handler;
 
+import br.com.patitas.app.service.exceptions.AppointmentStatusException;
 import br.com.patitas.app.service.exceptions.ResourceNotFoundException;
+import br.com.patitas.app.service.exceptions.ScheduleAlreadyUsedException;
+import br.com.patitas.app.service.exceptions.ScheduleDuplicatedException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +15,24 @@ import java.time.Instant;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<StandardError> runtimeExceptionHandler(
+            RuntimeException ex, HttpServletRequest request
+    ) {
+        StandardError error = new StandardError(
+                Instant.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<StandardError> resourceNotFoundExceptionHandler(ResourceNotFoundException ex, HttpServletRequest request) {
+    public ResponseEntity<StandardError> resourceNotFoundExceptionHandler(
+            ResourceNotFoundException ex, HttpServletRequest request
+    ) {
         StandardError error = new StandardError(
                 Instant.now(),
                 HttpStatus.NOT_FOUND.value(),
@@ -22,5 +41,47 @@ public class ControllerExceptionHandler {
                 request.getRequestURI()
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(AppointmentStatusException.class)
+    public ResponseEntity<StandardError> appointmentStatusExceptionHandler(
+            AppointmentStatusException ex, HttpServletRequest request
+    ) {
+        StandardError error = new StandardError(
+                Instant.now(),
+                HttpStatus.FORBIDDEN.value(),
+                "Forbidden",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
+    }
+
+    @ExceptionHandler(ScheduleAlreadyUsedException.class)
+    public ResponseEntity<StandardError> scheduleAlreadyUsedExceptionHandler(
+            ScheduleAlreadyUsedException ex, HttpServletRequest request
+    ) {
+        StandardError error = new StandardError(
+                Instant.now(),
+                HttpStatus.CONFLICT.value(),
+                "Conflict",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(ScheduleDuplicatedException.class)
+    public ResponseEntity<StandardError> scheduleDuplicatedExceptionHandler(
+            ScheduleDuplicatedException ex, HttpServletRequest request
+    ) {
+        StandardError error = new StandardError(
+                Instant.now(),
+                HttpStatus.CONFLICT.value(),
+                "Conflict",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 }
